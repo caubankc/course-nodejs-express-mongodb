a1c3e587abdc9fb1d8de80e4bd2e289e5b44b81f
ubankconnect <christian@ubankconnect.com>
2022-11-02 17:49:51 +0800

    started http module node

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var rect = require('./rectangle')
+const http = require('http')
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b)
-    rect(l, b, (err, rectangle) => {
-        if (err) {
-            console.log("ERROR: ", err.message)
-        } else {
-            console.log("The area of the rectangle of dimensions l = "+l+ " and b = " +b+ " is: " + rectangle.area())
-            console.log("The perimeter of the rectangle of dimensions l = "+l+ " and b = " +b+ " is: " + rectangle.perimeter())
-        }
-    })
+const hostname = 'localhost'
+const port = 3000
 
-    console.log("This statement is after the call to rect")
-}
+const server = http.createServer((req, res) => {
+    console.log(req.headers)
 
-solveRect(2,4)
-solveRect(3,5)
-solveRect(0,5)
-solveRect(-3,5)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><body><h1>Hello, world!</h1></body></html>')
+
+})
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
